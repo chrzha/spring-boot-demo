cd2d79cb04d13c187bd570078f0cda29a200360b
ChrisZhang <chrzha@localhost.localdomain>
2017-02-27 22:49:24 +0800

    first commit

---
+++ src/main/java/com/chrzha/demo/controller/FirstController.java
@@ -0,0 +1,26 @@
+package com.chrzha.demo.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by chrzha on 17-2-27.
+ */
+
+@Controller
+@EnableAutoConfiguration
+public class FirstController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(FirstController.class, args);
+    }
+}
